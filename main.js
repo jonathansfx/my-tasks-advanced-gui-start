b78ab38ec29d9a9385edd8ed4bed2784a9970a6a
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-04-12 11:22:00 -0600

    asdf

--- main.js
+++ main.js
@@ -1,54 +1,44 @@
 // My Tasks Basic
 
 // HTML Elements
-let goBtnEl = document.getElementById('go-btn');
-let menuEl = document.getElementById('menu');
-let tasksEl = document.getElementById('tasks');
+let taskInputEl = document.getElementById("task-input");
+let menuEl = document.getElementById("menu");
+let tasksEl = document.getElementById("tasks");
 
 // Global Variables
 let tasks = initTasks();
 displayTasks();
 
 // Go Btn - Menu Listener
-goBtnEl.addEventListener('click', goBtnHandler);
+taskInputEl.addEventListener("keydown", taskSubmitHandler);
 
-function goBtnHandler() {
-  // Get Menu Selection
-  let selection = menuEl.value;
-
-  if (selection === 'add') {
-    addTask();
-  } else if (selection === 'toggle') {
-    toggleTask();
-  } else if (selection === 'remove') {
-    removeTask();
-  } else if (selection === 'clear') {
-    clearAll();
+function taskSubmitHandler(e) {
+  console.log(e.code);
+  if (e.code === "Enter") {
+    // Add Submitted Task
+    let userTask = taskInputEl.value;
+    tasks.push(newTask(userTask));
+    saveTasks();
+    displayTasks();
+    taskInputEl.value = "";
   }
 }
 
 // MENU FUNCTIONS
-function addTask() {
-  let userTask = prompt('Please enter a new task:');
-  tasks.push(newTask(userTask));
-  saveTasks();
-  displayTasks();
-}
-
 function toggleTask() {
-  let taskIndex = +prompt('Please enter number of task to toggle:');
+  let taskIndex = +prompt("Please enter number of task to toggle:");
   let task = tasks[taskIndex];
-  if (task.completed === '') {
-    task.completed = 'completed';
+  if (task.completed === "") {
+    task.completed = "completed";
   } else {
-    task.completed = '';
+    task.completed = "";
   }
   saveTasks();
   displayTasks();
 }
 
 function removeTask() {
-  let taskIndex = +prompt('Please enter number of task to remove:');
+  let taskIndex = +prompt("Please enter number of task to remove:");
   tasks.splice(taskIndex, 1);
   saveTasks();
   displayTasks();
@@ -62,12 +52,12 @@ function clearAll() {
 
 // HELPERS
 function initTasks() {
-  let jsonTasks = localStorage.getItem('tasks');
+  let jsonTasks = localStorage.getItem("tasks");
   return JSON.parse(jsonTasks) ?? [];
 }
 
 function displayTasks() {
-  let outputStr = '';
+  let outputStr = "";
   for (let i = 0; i < tasks.length; i++) {
     outputStr += getTaskHTMLStr(tasks[i], i);
   }
@@ -77,18 +67,33 @@ function displayTasks() {
 function newTask(taskDescription) {
   return {
     description: taskDescription,
-    completed: '',
+    completed: "",
   };
 }
 
 function saveTasks() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function getTaskHTMLStr(task, index) {
-  return `
-    <div class="${task.completed}">
-      ${index}: ${task.description}
-    </div>
-  `;
+  // Use JavaScript to build the Task <div>
+
+  // Check Box Element
+  let checkBoxEl = document.createElement("input");
+  checkBoxEl.type = "checkbox";
+
+  // Text Description
+  let textEl = document.createTextNode(task.description);
+
+  // Remove Button
+  let buttonEl = document.createElement("button");
+  buttonEl.innerHTML = "Remove";
+
+  // Add everything to a div element
+  let divEl = document.createElement("div");
+  divEl.appendChild(checkBoxEl);
+  divEl.appendChild(textEl);
+  divEl.appendChild(buttonEl);
+
+  return divEl;
 }
